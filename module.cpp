43ef907a26e1354579b4286dcea5097cf6534d1c
Nick <nick.benoit14@gmail.com>
2016-06-16 08:14:32 -0600

    implmented base receive, broadcast

---
+++ module.cpp
@@ -0,0 +1,27 @@
+#include "module.hpp"
+
+Module::Module(Controller* control):controller(control){}
+
+bool Module::receive(Message* message){
+    if(!message){return 0;}
+
+    unsigned int action = message->action;
+    std::string body = message->body;
+
+    switch(action){
+      case EXAMPLE_ACTION:
+        std::cout << "Example Action Received\n";
+      break;
+
+      default:
+        std::cout << "Unmatching Message Recieved\n";
+    }
+    return 1;
+}
+
+bool Module::broadcast(Message* message){
+  if(!controller || !message){return 0;}
+
+  controller->broadcast(message);
+  return 1;
+}
