50d509e0595f12b27a1fd1a8ccac45e85dd3c759
Nick <nick.benoit14@gmail.com>
2016-06-22 19:14:54 -0600

    add read loop

--- main.cpp
+++ main.cpp
@@ -6,18 +6,19 @@
 #include "modules/CameraModule/CameraModule.hpp"
 
 int main(void){
-	Controller controller;
-	CameraModule mod;
-	controller.addModule(&mod);
-	mod.takePhoto();
-	sleep(10);
+	// Controller controller;
+	// CameraModule mod;
+	// controller.addModule(&mod);
+	// mod.takePhoto();
+	// sleep(10);
 
-  // CameraModule camera;
-	//
-  // controller.addModule(&camera);
-	//
-  // while(true){
-  //   controller.readModules();
-  // }
+	Controller controller;
+  CameraModule camera;
+
+  controller.addModule(&camera);
+
+  while(true){
+    controller.readModules();
+  }
   return 0;
 }
